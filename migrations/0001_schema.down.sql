6647b8d08e6bdb9e4e92759c2361cfd445f38cc6
Christian Brennecke-Räther <c.b.raether@gmail.com>
2023-01-31 20:48:26 +0100

    DO-52 add analyzer db schema & migrate to compose file

---
+++ migrations/0001_schema.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS analysis_processes;
+DROP TABLE IF EXISTS documents;
+
+DROP TYPE IF EXISTS key_value_pair;
+DROP TYPE IF EXISTS analysis_status;
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
--- migrations/schema.v1.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE If EXISTS album;
-CREATE TABLE album (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(128) NOT NULL,
-  artist VARCHAR(255) NOT NULL,
-  price DECIMAL(5, 2) NOT NULL
-);
